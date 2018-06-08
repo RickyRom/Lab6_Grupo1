bd24fdd797c8deb3765ef08bbae757639eefbe88
3ddyRomero <eduardromero@unitec.edu>
2018-06-08 13:55:47 -0600

    primer commit

---
+++ Dinosaurios.h
@@ -0,0 +1,17 @@
+#ifndef DINOSAURIOS_H
+#define DINOSAURIOS_H
+
+#include <iostream>
+using namespace std;
+
+class Dinosaurios{
+        public:
+            string Nombre;
+            double altura, peso;
+
+
+        private:
+
+
+};
+#endif
